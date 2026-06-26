8a2f66960560f012e0386715160ec0cd7836d738
learnautomation1604-cyber <learnautomation1604@gmail.com>
2026-06-26 23:56:50 +0530

    Today's commit_06262026

---
+++ src/test/java/Main.java
@@ -0,0 +1,11 @@
+class Employee {
+
+    void student() {
+        System.out.println("Hey I am the student Mythili");
+    }
+
+        public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.student();
+    }
+}
