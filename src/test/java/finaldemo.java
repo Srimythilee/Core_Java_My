6bb13cc0ec5ecadc4739904cbd6f30a46eb10189
learnautomation1604-cyber <learnautomation1604@gmail.com>
2026-07-07 00:13:19 +0530

    todayscommitforJava=Aray in detail_07072026

---
+++ src/test/java/finaldemo.java
@@ -0,0 +1,35 @@
+// In java the final keyword is used to restrict modifications . It can be applied variables, methods, classes
+// final variable - A final variable cannot be reassigned once it has been initialized
+// , final method - A final method canot be Overridden by  subclasses
+// , final class - A final class cannot be inherited
+
+
+ class classname{
+
+
+     final void addition(){
+
+    }
+
+
+      class demon extends  classname{
+
+
+    }
+
+    int addition(int v){
+
+        return 0;
+    }
+
+    static void main() {
+
+        final  int x =10;
+        int y = 20;
+        double z = x;
+
+
+
+
+    }
+}
